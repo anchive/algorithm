024892a3d33644eba82e8ab7346bd29f6d00fdcc
anchive <dkslrjsl@gmail.com>
2022-10-04 16:00:36 +0900

    11724 fix

--- acmicpc/python/11724 연결 요소의 개수.py
+++ acmicpc/python/11724 연결 요소의 개수.py
@@ -1,27 +1,28 @@
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 
-graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
+dic = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+count = 0
 
-for _ in range(M):
-    x, y = map(int, input().split())
-    graph[x] += [y]
-    graph[y] += [x]
+for _ in range(m):
+    a, b = map(int, input().split())
+    dic[a] += [b]
+    dic[b] += [a]
 
 def dfs(v):
     visited[v] = True
-    for i in graph[v]:
+    for i in dic[v]:
         if visited[i] == False:
             visited[i] = True
             dfs(i)
 
-for i in range(1, N+1):
+for i in range(1, n+1):
     if visited[i] == False:
         count += 1
         dfs(i)
-        print('visited:',visited)
 
 print(count)
