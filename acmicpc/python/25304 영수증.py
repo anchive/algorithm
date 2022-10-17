7d6aaf33c981de8b78e7344e29225260a1cce7fb
anchive <dkslrjsl@gmail.com>
2022-10-17 17:17:32 +0900

    25304

---
+++ acmicpc/python/25304 영수증.py
@@ -0,0 +1,12 @@
+answer = int(input())
+n = int(input())
+total = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    total += a*b
+
+if total == answer:
+    print('Yes')
+else:
+    print('No')
