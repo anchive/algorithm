6a76b721b749acabd317dd8d1fdd76a8f95625d8
anchive <dkslrjsl@gmail.com>
2022-10-25 08:13:26 +0900

    10807, 2562

---
+++ acmicpc/python/correct answer/10807 개수 세기.py
@@ -0,0 +1,4 @@
+n = int(input())
+li = list(map(int, input().split()))
+find = int(input())
+print(li.count(find))
---
+++ acmicpc/python/correct answer/2562 최댓값.py
@@ -0,0 +1,4 @@
+li = [int(input()) for i in range(9)]
+
+print(max(li))
+print(li.index(max(li))+1)
